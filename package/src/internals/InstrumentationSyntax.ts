51455bae94d4df74774d3ed14fe187ed878f4951
Eduard Malakhov <eduard@atweel.com>
2020-07-17 01:35:49 +0700

    fix: add missing type specifications for *Syntax

--- package/src/internals/InstrumentationSyntax.ts
+++ package/src/internals/InstrumentationSyntax.ts
@@ -1,6 +1,6 @@
 import { InstrumentationLike, InstrumentationParametersOf, InstrumentationResultOf } from '~/types';
 
-interface InstrumentationSyntax<I extends InstrumentationLike<I>, R extends object = {}> {
+interface InstrumentationSyntax<I extends InstrumentationLike<I, object, any[], any>, R extends object = {}> {
     with<K extends Exclude<keyof I, symbol>>(instrumentation: K, ...parameters: InstrumentationParametersOf<I, K>): InstrumentationSyntax<I, R & Record<K, InstrumentationResultOf<I, K>>>;
     complete(): R;
 }
